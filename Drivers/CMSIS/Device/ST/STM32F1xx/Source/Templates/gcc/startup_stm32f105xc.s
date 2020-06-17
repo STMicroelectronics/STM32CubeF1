b9f10af06c8dc544a81dcf05dc02d60bf80ebc64
Flávio Tapajós <flavio.tapajos@newtesc.com.br>
2020-06-17 18:09:56 -0300

    Sets stack pointer on startup

--- Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/startup_stm32f105xc.s
+++ Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/startup_stm32f105xc.s
@@ -60,6 +60,7 @@ defined in linker script */
   .weak Reset_Handler
   .type Reset_Handler, %function
 Reset_Handler:
+  ldr   sp, =_estack       /* set stack pointer */
 
 /* Copy the data segment initializers from flash to SRAM */
   movs r1, #0
